62a3beb87a283096db25360e934a8b1044e6b74d
Silambarasan <msilambarasan98@gmail.com>
2023-07-22 16:32:27 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace Student_data_Project
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string path = "E:\\Project\\Project 1\\Student Details.txt";
+            string studentfile = File.ReadAllText(path);
+            Console.WriteLine(studentfile);
+        }
+    }
+}
+
+
